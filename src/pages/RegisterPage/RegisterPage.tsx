89f251b613cf1f170301f8b5c32ee929dcf2540a
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2023-08-11 20:32:12 +0300

    add style

--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -3,6 +3,8 @@ import { FC } from "react";
 import Logo from "@/src/components/SVGComponents/Logo";
 import RegisterForm from "./components/RegisterForm/RegisterForm";
 
+import styles from "./RegisterPage.module.scss";
+
 const RegisterPage: FC = () => {
   const renderFields = [
     {
@@ -15,8 +17,8 @@ const RegisterPage: FC = () => {
 
   return (
     <>
-      <Logo className="md:block" />
-      <div>
+      <Logo className="md:block fixed top-[70px] left-[70px] hidden" />
+      <div className={styles["regPage-container"]}>
         <h1>Sign up</h1>
         <RegisterForm formName="register" fields={renderFields} />
       </div>
