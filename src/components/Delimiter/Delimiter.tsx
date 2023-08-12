13cc0215f083300c9a15502a267da83224bad19b
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2023-08-12 13:46:41 +0300

    edit delimiter tags

--- src/components/Delimiter/Delimiter.tsx
+++ src/components/Delimiter/Delimiter.tsx
@@ -2,9 +2,9 @@ import styles from "./Delimiter.module.scss";
 
 const Delimiter = () => {
   return (
-    <div className={styles["loginPage_delimiter"]}>
+    <div className={styles["authPage_delimiter"]}>
       <div className={styles["delimiter-line"]}></div>
-      <div className="text-[20px]">or</div>
+      <p className="text-[20px]">or</p>
       <div className={styles["delimiter-line"]}></div>
     </div>
   );
