c83c88c278ce45ebd45b9ea0872b6729ae6e713f
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2023-08-14 11:50:04 +0300

    refactor/delimiter-styles

--- src/components/Delimiter/Delimiter.tsx
+++ src/components/Delimiter/Delimiter.tsx
@@ -3,7 +3,7 @@ import styles from "./Delimiter.module.scss";
 const Delimiter = ({ text = "or" }) => {
   return (
     <div className={`${styles["delimiter"]}`}>
-      <p className={`${styles["delimiter__line"]} text-[20px]`}>{text}</p>
+      <p className={`${styles["delimiter__line"]}`}>{text}</p>
     </div>
   );
 };
