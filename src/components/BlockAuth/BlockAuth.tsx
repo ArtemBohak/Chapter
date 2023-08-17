de6a91a8cc8c8b96b799e396197021f8aa318ef2
vasilkovalyov <vasilkovalyov@gmail.com>
2023-08-17 20:33:44 +0300

    refactored BlockAuth, replaced types to separate file, removed useless cn for classnames

--- src/components/BlockAuth/BlockAuth.tsx
+++ src/components/BlockAuth/BlockAuth.tsx
@@ -1,16 +1,11 @@
 import { FC } from "react";
 import cn from "classnames";
 
+import { BlockAuthProps } from "./BlockAuth.type";
+
 import styles from "./BlockAuth.module.scss";
 
-type Props = {
-  className?: string;
-  children: React.ReactNode;
-  heading?: string;
-  showBottomText?: boolean;
-};
-
-const BlockAuth: FC<Props> = ({
+const BlockAuth: FC<BlockAuthProps> = ({
   className,
   heading,
   showBottomText = false,
@@ -18,15 +13,15 @@ const BlockAuth: FC<Props> = ({
 }) => {
   return (
     <section className={cn(styles["block-auth"], className)}>
-      <div className={cn(styles["block-auth__container"])}>
-        <div className={cn(styles["block-auth__body"])}>
+      <div className={styles["block-auth__container"]}>
+        <div className={styles["block-auth__body"]}>
           {heading && (
-            <h1 className={cn(styles["block-auth__heading"])}>{heading}</h1>
+            <h1 className={styles["block-auth__heading"]}>{heading}</h1>
           )}
           {children}
         </div>
         {showBottomText ? (
-          <div className={cn(styles["block-auth__bottom-text"])}>
+          <div className={styles["block-auth__bottom-text"]}>
             <p>
               By clicking “Create account” above, you acknowledge that you have
               read and understood, and agree to Chapter's
