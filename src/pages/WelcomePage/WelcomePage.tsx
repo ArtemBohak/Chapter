a55ca579f78b87b4a4d683629a2eeff9583531e5
vasilkovalyov <vasilkovalyov@gmail.com>
2023-08-18 14:58:44 +0300

    removed useless logo from WelcomePage

--- src/pages/WelcomePage/WelcomePage.tsx
+++ src/pages/WelcomePage/WelcomePage.tsx
@@ -1,6 +1,5 @@
 import { FC } from "react";
 
-import Logo from "@/src/components/SVGComponents/Logo";
 import styles from "./WelcomePage.module.scss";
 
 import WelcomePageImage from "./components/WelcomePageImage/WelcomePageImage";
@@ -9,7 +8,6 @@ import WelcomePagePanel from "./components/WelcomePagePanel/WelcomePagePanel";
 const WelcomePage: FC = () => {
   return (
     <>
-      <Logo className={styles.logo} />
       <div className={styles["welcome-page__container"]}>
         <WelcomePageImage />
         <WelcomePagePanel />
