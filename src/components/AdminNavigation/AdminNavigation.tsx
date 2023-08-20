be616dad88b27e1db9ce2abb9d2bd6e5ce9b9793
vasilkovalyov <vasilkovalyov@gmail.com>
2023-08-20 18:49:03 +0300

    refactored AdminNavigation, created NavigationList component

--- src/components/AdminNavigation/AdminNavigation.tsx
+++ src/components/AdminNavigation/AdminNavigation.tsx
@@ -1,49 +1,17 @@
 import { FC } from "react";
-import { Link } from "react-router-dom";
-import cn from "classnames";
-
-import styles from "./AdminNavigation.module.scss";
-
-import { NavigationLinkProps } from "./AdminNavigation.type";
-import { Icon } from "../Icon";
+import { NavigationList } from "../NavigationList";
 
 import { topNavigation, bottomNavigation } from "./AdminNavigation.data";
-
-function NavList({
-  className,
-  items,
-}: {
-  className?: string;
-  items: NavigationLinkProps[];
-}) {
-  return (
-    <ul className={cn(styles["admin-navigation__list"], className)}>
-      {items.map((navItem) => (
-        <li key={navItem.id} className={styles["admin-navigation__item"]}>
-          <Link
-            to={navItem.path}
-            className={cn(styles["admin-navigation__link"])}
-          >
-            <Icon
-              icon={navItem.icon}
-              className={styles["admin-navigation__link-icon"]}
-            />
-            {navItem.name}
-          </Link>
-        </li>
-      ))}
-    </ul>
-  );
-}
+import styles from "./AdminNavigation.module.scss";
 
 const AdminNavigation: FC = () => {
   return (
     <nav className={styles["admin-navigation"]}>
-      <NavList
+      <NavigationList
         className={styles["admin-navigation__top-nav-list"]}
         items={topNavigation}
       />
-      <NavList
+      <NavigationList
         className={styles["admin-navigation__bottom-nav-list"]}
         items={bottomNavigation}
       />
