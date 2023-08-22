7c703baaaa05c5687f800f665668d256da2749d6
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2023-08-22 18:50:01 +0300

    refactor: making changes to the errro page, after the changes of ui-kit

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,5 +1,4 @@
 import { FC } from "react";
-import { useNavigate } from "react-router-dom";
 
 import image from "@/src/assets/WEBP/page-404.webp";
 import styles from "./ErrorPage.module.scss";
@@ -7,42 +6,41 @@ import styles from "./ErrorPage.module.scss";
 import { ErrorLayout } from "./components";
 import { UIbutton } from "@/src/components";
 
-const ErrorPage: FC = () => {
-  const navigate = useNavigate();
-  return (
-    <ErrorLayout>
-      <section className={styles["block-error"]}>
-        <div className={styles["block-error__container"]}>
-          <div className={styles["block-error__thumb"]}>
-            <img
-              src={image}
-              alt="error-404"
-              width="974"
-              height="649"
-              className={styles["block-error__image"]}
-            />
-          </div>
-          <div className={styles["block-error__content"]}>
-            <div className={styles["block-error__wrapper"]}>
-              <h1 className={styles["block-error__heading"]}>
-                Oh no! Page not found
-              </h1>
-              <p className={styles["block-error__text"]}>
-                Sorry, we couldn`t find the page you are looking for.
-              </p>
-            </div>
-            <UIbutton
-              onClick={() => navigate("/")}
-              dataAutomation="navigationButton"
-              className={styles["block-error__button"]}
-            >
-              Go to home page
-            </UIbutton>
-          </div>
+const ErrorPage: FC = () => (
+  <ErrorLayout>
+    <section className={styles["block-error"]}>
+      <div className={styles["block-error__container"]}>
+        <div className={styles["block-error__thumb"]}>
+          <img
+            src={image}
+            alt="error-404"
+            width="974"
+            height="649"
+            className={styles["block-error__image"]}
+          />
         </div>
-      </section>
-    </ErrorLayout>
-  );
-};
+        <div className={styles["block-error__content"]}>
+          <div className={styles["block-error__wrapper"]}>
+            <h1 className={styles["block-error__heading"]}>
+              Oh no! Page not found
+            </h1>
+            <p className={styles["block-error__text"]}>
+              Sorry, we couldn`t find the page you are looking for.
+            </p>
+          </div>
+          <UIbutton
+            href="/"
+            variant="contained"
+            dataAutomation="navigationButton"
+            size="large"
+            // className={styles["block-error__button"]}
+          >
+            Go to home page
+          </UIbutton>
+        </div>
+      </div>
+    </section>
+  </ErrorLayout>
+);
 
 export default ErrorPage;
