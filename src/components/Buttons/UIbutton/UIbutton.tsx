7f8866f07e27fbf2b38d5a2f78ea43eff092acbc
Vasiliy <vasilkovalyov@gmail.com>
2023-08-22 13:08:41 +0300

    Fix/basic UI kit (#14)
    * refactored typography
    
    * refactored tailwind config, refactored button, updated styles

--- src/components/Buttons/UIbutton/UIbutton.tsx
+++ src/components/Buttons/UIbutton/UIbutton.tsx
@@ -1,35 +1,86 @@
 import { FC } from "react";
 import cn from "classnames";
 
-import { type UIButtonProps } from "@/src/components/Buttons/UIbutton/ui-button.type";
-import styles from "./UIbutton.module.scss";
+import { Icon } from "../../Icon";
+import { UIButtonProps } from "./ui-button.type";
+
+import "./UIbutton.scss";
+import { Link } from "react-router-dom";
 
 const UIbutton: FC<UIButtonProps> = ({
   isLoading = false,
   className,
   children,
-  variant,
-  dataAutomation,
+  fullWidth,
+  color = "primary",
+  variant = "contained",
+  size = "small",
   type = "button",
+  icon,
+  isCustomIcon,
+  alignIcon,
+  dataAutomation,
+  href,
   ...props
 }) => {
-  const variantClassName: string =
-    (variant && styles[variant]) || styles["orange-contained"];
+  const variantClassName = cn({
+    "btn--contained": variant === "contained",
+    "btn--outlined": variant === "outlined",
+    "btn--text": variant === "text",
+  });
+
+  const sizeClassName = cn({
+    "btn--small": size === "small",
+    "btn--medium": size === "medium",
+    "btn--large": size === "large",
+  });
+
+  const colorClassName = cn({
+    "btn--primary": color === "primary",
+  });
+
+  const alignIconClassName = cn({
+    "btn--with-icon": icon || isCustomIcon,
+    "btn--icon-left": alignIcon === "left",
+    "btn--icon-right": alignIcon === "right",
+  });
+
+  const baseClassName = cn(
+    "btn",
+    variantClassName,
+    colorClassName,
+    sizeClassName,
+    alignIconClassName,
+    {
+      "btn--loading": isLoading,
+    },
+    {
+      "btn--fullwidth": fullWidth,
+    },
+    className
+  );
+
+  if (href) {
+    return (
+      <Link
+        to={href}
+        data-automation={dataAutomation}
+        className={baseClassName}
+      >
+        {icon ? <Icon className="btn__icon" icon={icon} /> : null}
+        {children}
+      </Link>
+    );
+  }
 
   return (
     <button
       type={type}
       data-automation={dataAutomation}
-      className={cn(
-        styles["btn"],
-        variantClassName,
-        {
-          [styles["btn_loading"]]: isLoading,
-        },
-        className
-      )}
+      className={baseClassName}
       {...props}
     >
+      {icon ? <Icon className="btn__icon" icon={icon} /> : null}
       {children}
     </button>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,3 +2,4 @@ export { default as WelcomePage } from "./WelcomePage/WelcomePage";
 export { default as RegisterPage } from "./RegisterPage/RegisterPage";
 export { default as AccountCreationPage } from "./AccountCreationPage/AccountCreationPage";
 export { default as LoginPage } from "./LoginPage/LoginPage";
+export { default as UIPage } from "./UIPage/UIPage";
