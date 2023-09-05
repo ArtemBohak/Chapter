4b799a2fe1016c7ffdb3a5a87284f3ae1bffc7ee
ArtemBohak <abogak3@gmail.com>
2023-09-05 23:56:04 +0300

    Revert "testing aws"
    This reverts commit a4e3d7f8deb52b0cf9392efa79a401447ab50180.

--- src/pages/WelcomePage/components/WelcomePagePanel/WelcomePagePanel.tsx
+++ src/pages/WelcomePage/components/WelcomePagePanel/WelcomePagePanel.tsx
@@ -21,7 +21,7 @@ const WelcomePagePanel: FC = () => {
             dataAutomation="navigationButton"
             href={links.SIGN_UP}
           >
-            Sign updfdsfsdf
+            Sign up
           </UIbutton>
           <UIbutton
             size="large"
