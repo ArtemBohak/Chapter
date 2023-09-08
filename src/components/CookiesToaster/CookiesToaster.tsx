3018efcb452731a2949a63a73bccaefbc3d51ee9
BogAlex4ndr <bog.alex4ndr@gmail.com>
2023-09-08 21:43:14 +0300

    Create Cookie toaster, add key to LocStor

--- src/components/CookiesToaster/CookiesToaster.tsx
+++ src/components/CookiesToaster/CookiesToaster.tsx
@@ -1,11 +1,26 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import CookiesImage from "@/src/assets/WEBP/CookiesGirl.webp";
 import styles from "./CookiesToaster.module.scss";
 import { Icon, IconEnum } from "..";
 
 const CookiesToaster: FC = () => {
+  const [toasterHide, setToasterHide] = useState(false);
+
+  useEffect(() => {
+    const acceptedCookies = localStorage.getItem("CookieAccept");
+    return () => {
+      if (acceptedCookies === "1") {
+        setToasterHide(true);
+      }
+    };
+  }, []);
+
+  const CookieAccept = () => {
+    localStorage.setItem("CookieAccept", "1");
+    setToasterHide(true);
+  };
   return (
-    <div className={styles["cookie-toaster__wrapper"]}>
+    <div hidden={toasterHide} className={styles["cookie-toaster__wrapper"]}>
       <div className={styles["info-block"]}>
         <h5 className={styles["info-block__title"]}>
           This website uses cookies
@@ -15,7 +30,7 @@ const CookiesToaster: FC = () => {
           to browse on this website, you agree to our use of cookies.
         </p>
 
-        <div className={styles["info-block__accept"]}>
+        <div onClick={CookieAccept} className={styles["info-block__accept"]}>
           <p>Continue to website</p>
           <Icon width={20} height={20} icon={IconEnum.ArrowRight} />
         </div>
