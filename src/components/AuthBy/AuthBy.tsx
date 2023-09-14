32deaf9063e4e021748fd2723688f15b68a21953
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2023-09-14 09:54:15 +0300

    feat: this add oAuth component to AuthBy

--- src/components/AuthBy/AuthBy.tsx
+++ src/components/AuthBy/AuthBy.tsx
@@ -3,12 +3,16 @@ import { FC } from "react";
 import { type AuthByProps } from "./AuthBy.type";
 import styles from "./AuthBy.module.scss";
 
-import { OAuthContainer } from "../OAuth";
+import { OAuth, OAuthContainer } from "../OAuth";
 
 const AuthBy: FC<AuthByProps> = ({ className }) => (
   <div className={`${styles["auth-by"]} ${className}`}>
     <OAuthContainer>
-      <div className={styles["auth-by__container"]}></div>
+      <div className={styles["auth-by__container"]}>
+        <OAuthContainer>
+          <OAuth oAuthVariant="google" />
+        </OAuthContainer>
+      </div>
     </OAuthContainer>
   </div>
 );
