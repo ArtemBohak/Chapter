a2370720cd0691a3ffe002165adbd77c8aeabf15
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2023-09-15 09:53:39 +0300

    fix: api base url local env name

--- src/axios/api.ts
+++ src/axios/api.ts
@@ -3,8 +3,7 @@ import TokenService from "@/src//services/token";
 import { getTokenFromLC } from "@/src/utils/localstorage";
 
 const api = axios.create({
-  //will be changed to const from .env later
-  baseURL: import.meta.env.VITE_REACT_API_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: false,
   method: "get, post, put, delete, patch",
   headers: {
