cb9454d7b64d64b115cd449f7521c81dfeed9c7c
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2023-10-11 17:19:34 +0300

    feat: this add redirecting url to home page

--- src/containers/Router/Routes/PrivateRoute/PrivateRoute.tsx
+++ src/containers/Router/Routes/PrivateRoute/PrivateRoute.tsx
@@ -3,13 +3,14 @@ import { Navigate } from "react-router-dom";
 
 import { RoutesProps } from "../types/Routes.type";
 import { useAppSelector } from "@/src/redux/hooks";
-import { links } from "@/src/utils";
+import { getTokenFromLC, links } from "@/src/utils";
 
 const PrivateRoute: FC<RoutesProps> = ({
   component: Component,
   redirectUrl = links.LOG_IN,
 }) => {
   const { isAuth } = useAppSelector((state) => state.userSlice);
+  if (!getTokenFromLC()) return <Navigate to="/" replace />;
 
   return isAuth ? Component : <Navigate to={redirectUrl} replace />;
 };
