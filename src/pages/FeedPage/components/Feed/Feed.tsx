be6304ef137ff26b50aa8c9de47bcef014663fd7
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2023-11-01 13:47:55 +0200

    feat: this add feed text markup

--- src/pages/FeedPage/components/Feed/Feed.tsx
+++ src/pages/FeedPage/components/Feed/Feed.tsx
@@ -7,18 +7,27 @@ import User from "./components/User/User";
 import FollowButton from "./components/FollowButton/FollowButton";
 import FeedImage from "./components/FeedImage/FeedImage";
 import FeedActivity from "./components/FeedActivity/FeedActivity";
+import FeedText from "./components/FeedText/FeedText";
 
 const Feed: FC<FeedProps> = (props) => {
   return (
     <div className={styles["feed-item"]}>
-      <div className={styles["feed-item__top-container"]}>
-        <User {...props} />
-        <FollowButton {...props} />
+      <div
+        className={`${styles["feed-item__wrapper"]} ${styles["feed-item__wrapper--top"]}`}
+      >
+        <div className={styles["feed-item__top-container"]}>
+          <User {...props} />
+          <FollowButton {...props} />
+        </div>
+        <div className={styles["feed-item__image-wrapper"]}>
+          <FeedImage {...props} />
+          <FeedActivity {...props} />
+        </div>
+        <FeedText {...props} />
       </div>
-      <div className={styles["feed-item__image-thumb"]}>
-        <FeedImage {...props} />
-        <FeedActivity {...props} />
-      </div>
+      <div
+        className={`${styles["feed-item__wrapper"]} ${styles["feed-item__wrapper--bottom"]}`}
+      ></div>
     </div>
   );
 };
--- src/pages/FeedPage/components/Feed/components/Feedtext/FeedText.tsx
+++ src/pages/FeedPage/components/Feed/components/Feedtext/FeedText.tsx
@@ -1,7 +1,37 @@
-import { FC } from "react";
+import { FC, useRef, useState } from "react";
 
-import { FeedTextProps } from "./FeedText.type";
+import "@/src/extensions/string.extensions";
+import { FeedTextProps, Words } from "./FeedText.type";
+import styles from "./FeedText.module.scss";
 
-const FeedText: FC<FeedTextProps> = ({ title, text }) => {};
+const FeedText: FC<FeedTextProps> = ({ title, text }) => {
+  const { current: screenSize } = useRef(window.innerWidth);
+  const [isShowingFullText, setIsShowingFullText] = useState(false);
+
+  const textSize =
+    screenSize < Words.ScreenSize ? Words.MobWords : Words.TabletWords;
+
+  const onHandleClick = () => {
+    setIsShowingFullText(!isShowingFullText);
+  };
+
+  const renderText = isShowingFullText ? text : text.limit(textSize);
+
+  const renderButton =
+    text.split(" ").length > textSize ? (
+      <button data-automation="clickButton" onClick={onHandleClick}>
+        {isShowingFullText ? "Read less" : "Read more"}
+      </button>
+    ) : null;
+
+  return (
+    <div className={styles["feed-text"]}>
+      <h4>{title}</h4>
+      <p>
+        {renderText} {renderButton}
+      </p>
+    </div>
+  );
+};
 
 export default FeedText;
