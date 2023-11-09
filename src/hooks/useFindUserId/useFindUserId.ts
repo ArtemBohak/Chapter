d6891e902fa6a6c4c5303127ff6ac45da94dd1fa
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2023-11-09 20:53:54 +0200

    refactor: change useFinUserId hook

--- src/hooks/useFindUserId/useFindUserId.ts
+++ src/hooks/useFindUserId/useFindUserId.ts
@@ -4,13 +4,15 @@ import { useAppSelector } from "@/src/redux";
 
 export const useFindUserId = (data: dataList) => {
   const { id } = useAppSelector((state) => state.userSlice.user);
-  const isIncluded = useMemo(() => {
-    const dataList = data ? data : [];
-    return dataList.some((i) => {
-      if (typeof i === "object" && i.id) return i.id + "" === id + "";
-      return i + "" === id + "";
-    });
-  }, [data, id]);
 
-  return [isIncluded];
+  return [
+    useMemo(
+      () =>
+        data.some((i) => {
+          if (typeof i === "object" && i.id) return i.id + "" === id + "";
+          return i + "" === id + "";
+        }),
+      [data, id]
+    ),
+  ];
 };
