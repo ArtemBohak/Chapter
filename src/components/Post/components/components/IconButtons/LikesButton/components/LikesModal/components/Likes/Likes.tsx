fe1b552b62077804ce71345b8225b8fbcd9d43d0
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2023-11-14 11:50:31 +0200

    feat: this add user filtration at likes component

--- src/components/Post/components/components/IconButtons/LikesButton/components/LikesModal/components/Likes/Likes.tsx
+++ src/components/Post/components/components/IconButtons/LikesButton/components/LikesModal/components/Likes/Likes.tsx
@@ -1,14 +1,23 @@
-import { FC } from "react";
+import { FC, useMemo } from "react";
 
 import { LikesProps } from "./Likes.type";
 import styles from "./Likes.module.scss";
 
 import { Like } from "./components";
+import { useAppSelector } from "@/src/redux";
 
 const Likes: FC<LikesProps> = ({ likesData = [], ...props }) => {
+  const {
+    user: { id },
+  } = useAppSelector((state) => state.userSlice);
+
+  const likes = useMemo(
+    () => likesData.filter((i) => i.id !== id),
+    [id, likesData]
+  );
   return (
     <ul className={styles["likes-list"]}>
-      {likesData.map((i) => (
+      {likes.map((i) => (
         <li key={i.id}>
           <Like {...i} {...props} />
         </li>
