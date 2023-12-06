c55a46c06cdab9a6423f6d3d7c3e8a5964611e4e
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2023-12-06 17:00:57 +0200

    feat: this add errorBoundary markup, style and functional

--- src/pages/ErrorBoundary/ErrorBoundary.tsx
+++ src/pages/ErrorBoundary/ErrorBoundary.tsx
@@ -1,21 +1,43 @@
-import { ErrorLayout } from "@/src/layouts";
 import { FC } from "react";
-import { Link, isRouteErrorResponse, useRouteError } from "react-router-dom";
+import {
+  isRouteErrorResponse,
+  useNavigate,
+  useRouteError,
+} from "react-router-dom";
 
-// import styles from "./ErrorBoundary.module.scss";
+import styles from "./ErrorBoundary.module.scss";
+import { ErrorLayout } from "@/src/layouts";
+import { UIbutton } from "@/src/components";
 
 const ErrorBoundary: FC = () => {
   const error = useRouteError();
+  const navigate = useNavigate();
 
   if (!isRouteErrorResponse(error))
     return (
       <ErrorLayout>
-        <section>
-          <h1>Something wrong!!!</h1>
-          <Link to={"/"}>HOME</Link>
+        <section className={styles["error"]}>
+          <div className={styles["error__wrapper"]}>
+            <h2 className={`${styles["error__title"]} ${styles["title"]}`}>
+              Oh no! Something went wrong!
+            </h2>
+            <div className={styles["error__image"]}></div>
+            <div className={styles["error__button"]}>
+              <UIbutton
+                dataAutomation="clickButton"
+                fullWidth
+                onClick={() => navigate("/")}
+                className={`${styles["error__button"]} ${styles["button"]}`}
+              >
+                Go to home page
+              </UIbutton>
+            </div>
+          </div>
         </section>
       </ErrorLayout>
     );
+
+  return null;
 };
 
 export default ErrorBoundary;
