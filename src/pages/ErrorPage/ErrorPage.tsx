6eaef977a0b6dc1e9c31cbbba97cba0c099c0571
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2023-12-06 20:56:31 +0200

    style: change errorPage styles

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -7,36 +7,24 @@ import { ErrorLayout } from "@/src/layouts";
 
 import { UIbutton } from "@/src/components";
 
-import image from "@/src/assets/WEBP/page-404.webp";
-
 const ErrorPage: FC = () => {
   const navigate = useNavigate();
   return (
     <ErrorLayout>
-      <section className={styles["block-error"]}>
-        <div className={styles["block-error__container"]}>
-          <div className={styles["block-error__thumb"]}>
-            <img
-              width="974"
-              height="649"
-              alt="error-404"
-              src={image}
-              className={styles["block-error__image"]}
-            />
-          </div>
-          <div className={styles["block-error__content"]}>
-            <div className={styles["block-error__wrapper"]}>
-              <h1 className={styles["block-error__heading"]}>
-                Oh no! Page not found
-              </h1>
-              <p className={styles["block-error__text"]}>
-                Sorry, we couldn`t find the page you are looking for.
-              </p>
-            </div>
+      <section className={styles["error"]}>
+        <div className={styles["error__container"]}>
+          <div className={styles["error__image"]}></div>
+          <h1 className={`${styles["error_title"]} ${styles["title"]}`}>
+            Oh no! Page not found
+          </h1>
+          <p className={`${styles["error__text"]} ${styles["text"]}`}>
+            Sorry, we couldn`t find the page you are looking for.
+          </p>
+          <div className={styles["error__button"]}>
             <UIbutton
               onClick={() => navigate("/")}
               dataAutomation="navigationButton"
-              className={`${styles["block-error__button"]} ${styles["error__btn"]}`}
+              className={`${styles["error__button"]} ${styles["button"]}`}
             >
               Go to home page
             </UIbutton>
