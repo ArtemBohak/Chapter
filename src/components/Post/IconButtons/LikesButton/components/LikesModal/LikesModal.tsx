f6d967730f16fa705cccd2cd8ebec10f569fa902
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2023-12-22 12:00:07 +0200

    refactor: showing add post btn login on mob screen

--- src/components/Post/IconButtons/LikesButton/components/LikesModal/LikesModal.tsx
+++ src/components/Post/IconButtons/LikesButton/components/LikesModal/LikesModal.tsx
@@ -1,8 +1,9 @@
 import { FC } from "react";
 
-import { useGetScreenSize } from "@/src/hooks";
+import { useGetScreenSize, useHideElement } from "@/src/hooks";
 import { LikesModalProps } from "./LikesModal.type";
 import { tabScreen } from "@/src/utils";
+import { ElementsId } from "@/src/types";
 import styles from "./LikesModal.module.scss";
 
 import { Modal, Icon, IconEnum } from "@/src/components";
@@ -16,6 +17,8 @@ const LikesModal: FC<LikesModalProps> = ({
   ...props
 }) => {
   const [screenSize] = useGetScreenSize();
+
+  useHideElement(ElementsId.ADD_POST_BTN, isOpen);
 
   const isMobScreen = screenSize < tabScreen;
 
