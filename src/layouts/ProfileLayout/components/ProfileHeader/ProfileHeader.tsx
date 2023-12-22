b42e97dd94c05cf8b6939b9b9ce57a6c518888b3
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2023-12-22 17:21:11 +0200

    refactor: this remove temporary thrown error button

--- src/layouts/ProfileLayout/components/ProfileHeader/ProfileHeader.tsx
+++ src/layouts/ProfileLayout/components/ProfileHeader/ProfileHeader.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from "react";
+import { FC } from "react";
 import { NavLink } from "react-router-dom";
 import cn from "classnames";
 
@@ -21,9 +21,6 @@ const ProfileHeader: FC<ProfileHeaderProps> = ({ setModalIsOpen }) => {
   const { isActiveMenu, setIsActiveMenu } = useNavigationToggler();
   const { user } = useAppSelector((store) => store.userSlice);
   const { firstName, lastName, avatarUrl } = user;
-  const [error, setError] = useState(false);
-
-  if (error) throw new Error("");
 
   useHideElement(ElementsId.ADD_POST_BTN, isActiveMenu);
 
@@ -117,14 +114,6 @@ const ProfileHeader: FC<ProfileHeaderProps> = ({ setModalIsOpen }) => {
               />
             </svg>
           </UIbutton>
-          <button
-            className="bg-red text-white"
-            onClick={() => {
-              setError(true);
-            }}
-          >
-            ERROR
-          </button>
         </div>
       </div>
     </header>
