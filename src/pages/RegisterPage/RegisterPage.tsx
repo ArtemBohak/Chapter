87687ac0b1a3af47ef8438eb590863d6e56070b1
ArtemBohak <115377006+ArtemBohak@users.noreply.github.com>
2023-12-26 12:38:54 +0200

    fix: left a cookie pop-up only on sign up page (#61)
    * fix: left a cookie pop-up only on sign up page
    
    * fix: left a cookie pop-up only on sign up page

--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -3,26 +3,30 @@ import { FC } from "react";
 import { links } from "@/src/types";
 import styles from "@/src/pages/RegisterPage/RegisterPage.module.scss";
 
-import { AuthLink, Delimiter, AuthBy, BlockAuth } from "@/src/components";
+import { AuthLink, Delimiter, AuthBy, BlockAuth, CookiesToaster } from "@/src/components";
 import { RegisterForm } from "@/src/pages/RegisterPage/components";
 
 const RegisterPage: FC = () => (
-  <BlockAuth
-    heading="Sign up"
-    showBottomText={true}
-    typePageText="Create new account"
-  >
-    <div className={styles["register-page"]}>
-      <RegisterForm />
-      <Delimiter />
-      <AuthBy />
-      <AuthLink
-        textMsg="Already have an account ?"
-        linkMsg="Log in"
-        link={links.LOG_IN}
-      />
-    </div>
-  </BlockAuth>
+  <>
+    <BlockAuth
+      heading="Sign up"
+      showBottomText={true}
+      typePageText="Create new account"
+      >
+      <div className={styles["register-page"]}>
+        <RegisterForm />
+        <Delimiter />
+        <AuthBy />
+        <AuthLink
+          textMsg="Already have an account ?"
+          linkMsg="Log in"
+          link={links.LOG_IN}
+          />
+      </div>
+    </BlockAuth>
+    <CookiesToaster />
+  </>
+
 );
 
 export default RegisterPage;
