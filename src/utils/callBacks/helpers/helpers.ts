15adec75e8e03c5b45ae8bf0c5abbf2540acccf8
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-03-05 09:14:41 +0200

    Update helpers.ts

--- src/utils/callBacks/helpers/helpers.ts
+++ src/utils/callBacks/helpers/helpers.ts
@@ -5,13 +5,15 @@ export const dataFindIndex = <T>(array: Array<T>, obj: T, key: keyof T) =>
 
 export const postDataEdit = <T>(data: Array<T>, pageLimit: number) => {
   return data.map((el, i) => {
-    if (data.length - 1 === i && data.length % pageLimit === 0) {
+    const pageValue = Math.round(data.length / pageLimit) + 1;
+
+    if (data.length - 1 === i && data.length % pageLimit === 0)
       return {
         ...el,
         nodeRef: createRef<HTMLDivElement>(),
-        pageValue: Math.round(data.length / pageLimit) + 1,
+        pageValue,
       };
-    }
+
     return {
       ...el,
       nodeRef: createRef<HTMLDivElement>(),
