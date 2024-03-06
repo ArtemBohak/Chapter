2756af7443c2772ad0b984475292101062c647c7
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-03-06 07:22:10 +0200

    refactor: this change post cb helpers

--- src/utils/callBacks/helpers/helpers.ts
+++ src/utils/callBacks/helpers/helpers.ts
@@ -1,20 +1,22 @@
 import { createRef } from "react";
+import { pageLimit } from "../..";
 
 export const dataFindIndex = <T>(array: Array<T>, obj: T, key: keyof T) =>
   array.findIndex((el) => el[key] === obj[key]);
 
-export const postDataEdit = <T>(data: Array<T>, pageLimit: number) => {
+export const postDataEdit = <T>(data: Array<T>, limit: number) => {
   return data.map((el, i) => {
-    const nextPage = Math.round(data.length / pageLimit) + 1;
-    const currentPage = Math.floor(i / pageLimit) + 1;
-    if (i === data.length - 1 && !(data.length % pageLimit))
+    const endIndex = limit === pageLimit ? 3 : 10;
+    const nextPage = Math.round(data.length / limit) + 1;
+    const currentPage = Math.floor(i / limit) + 1;
+    if (i === data.length - endIndex && !(data.length % limit))
       return {
         ...el,
         nodeRef: createRef<HTMLDivElement>(),
         pageValue: nextPage,
       };
 
-    if (!(i % 5))
+    if (!(i % 5) && i < data.length - endIndex)
       return {
         ...el,
         nodeRef: createRef<HTMLDivElement>(),
