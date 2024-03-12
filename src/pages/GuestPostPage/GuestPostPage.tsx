284dab8ba571a6dd54da35b83b5cd5aaef342806
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-03-12 06:57:34 +0200

    feat: this add useParams

--- src/pages/GuestPostPage/GuestPostPage.tsx
+++ src/pages/GuestPostPage/GuestPostPage.tsx
@@ -1,7 +1,11 @@
 import { FC } from "react";
+import { useParams } from "react-router-dom";
+
 import styles from "./GuestPostPage.module.scss";
 
 const GuestPostPage: FC = () => {
+  const { id } = useParams();
+  id;
   return (
     <section className={styles["post"]}>
       <div className={styles["container"]}>POST</div>
