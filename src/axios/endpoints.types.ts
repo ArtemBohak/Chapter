f4564a46d432712d89b51e9ce7bbe257ecced1d3
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-03-13 11:47:15 +0200

    fix: this fix post endpoint name

--- src/axios/endpoints.types.ts
+++ src/axios/endpoints.types.ts
@@ -40,7 +40,7 @@ export enum EndpointsEnum {
   // POSTS
   CREATE_POST = "posts/create",
   DELETE_POST = "posts/delete",
-  GET_POST = "post/",
+  GET_POST = "posts/post/",
   POSTS_BY_AUTHOR = "posts/by-author",
   POSTS_BY_USER = "posts/by-user/",
   LIKED_POSTS = "posts/posts",
