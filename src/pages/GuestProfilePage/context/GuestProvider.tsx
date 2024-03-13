e66a07f8bda352b3fcc86b48f77244a9f40d52eb
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-03-13 22:56:56 +0200

    feat: this add redirect logic on 404 error

--- src/pages/GuestProfilePage/context/GuestProvider.tsx
+++ src/pages/GuestProfilePage/context/GuestProvider.tsx
@@ -1,37 +1,41 @@
-import { FC, useEffect, useState } from 'react'
-import { GuestContext } from './hooks/useGuestContext'
-import { enemyData, guestProfileApi } from '../components';
-import { IGuestProviderProps } from './GuestProvider.type';
-import { useParams } from 'react-router-dom';
-import { EndpointsEnum, api } from '@/src/axios';
+import { FC, useEffect, useState } from "react";
+import { GuestContext } from "./hooks/useGuestContext";
+import { enemyData, guestProfileApi } from "../components";
+import { IGuestProviderProps } from "./GuestProvider.type";
+import { useNavigate, useParams } from "react-router-dom";
+import { EndpointsEnum, api } from "@/src/axios";
+import { links } from "@/src/types";
 
 const GuestProvider: FC<IGuestProviderProps> = ({ children }) => {
-  const { Id } = useParams()
+  const { Id } = useParams();
+  const navigate = useNavigate();
   const [enemyData, setEnemyData] = useState<enemyData>();
-  const [guestPostsList, setGuestPostsList] = useState([])
+  const [guestPostsList, setGuestPostsList] = useState([]);
 
   const fetchEnemyUserData = async () => {
     const response = await guestProfileApi(Id);
-    setEnemyData(response.data)
+    if (response.statusCode === 404) return navigate(links.HOME);
+
+    setEnemyData(response.data);
   };
 
   const fetchGuestPosts = async () => {
-    const response = await api.get(`${EndpointsEnum.POSTS_BY_USER}${Id}`)
-    setGuestPostsList(response.data)
-  }
+    const response = await api.get(`${EndpointsEnum.POSTS_BY_USER}${Id}`);
+    setGuestPostsList(response.data);
+  };
 
   useEffect(() => {
-    fetchEnemyUserData()
-    fetchGuestPosts()
-  }, [])
+    fetchEnemyUserData();
+    fetchGuestPosts();
+  }, []);
 
   return (
     <GuestContext.Provider
-      value={{ fetchEnemyUserData, guestPostsList, enemyData }}>
+      value={{ fetchEnemyUserData, guestPostsList, enemyData }}
+    >
       {children}
     </GuestContext.Provider>
+  );
+};
 
-  )
-}
-
-export default GuestProvider
+export default GuestProvider;
