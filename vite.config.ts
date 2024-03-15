525c440df016f20819336d466d46c8b2af0eb1a3
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-03-15 15:38:36 +0200

    feat: this add compresstion files plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import mkcert from "vite-plugin-mkcert";
+import viteCompression from "vite-plugin-compression";
 import * as path from "path";
 
 export default defineConfig({
   server: {
     https: true,
   },
-  plugins: [react(), mkcert()],
+  plugins: [
+    react(),
+    mkcert(),
+    viteCompression({ algorithm: "brotliCompress", deleteOriginFile: false }),
+  ],
   resolve: {
     alias: [
       { find: "@", replacement: path.resolve(__dirname) },
