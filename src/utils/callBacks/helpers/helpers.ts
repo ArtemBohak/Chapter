a4cd27981ffeef32dcaed290dee94862457d5747
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-03-17 22:09:41 +0200

    refactor: change post pagination logic

--- src/utils/callBacks/helpers/helpers.ts
+++ src/utils/callBacks/helpers/helpers.ts
@@ -1,15 +1,13 @@
 import { createRef } from "react";
-import { pageLimit } from "../..";
 
 export const dataFindIndex = <T>(array: Array<T>, obj: T, key: keyof T) =>
   array.findIndex((el) => el[key] === obj[key]);
 
 export const postDataEdit = <T>(data: Array<T>, limit: number) => {
   return data.map((el, i) => {
-    const endIndex = limit === pageLimit ? 3 : 10;
     const nextPage = Math.round(data.length / limit) + 1;
     const currentPage = Math.floor(i / limit) + 1;
-    if (i === data.length - endIndex && !(data.length % limit))
+    if (i === data.length - 1 && !(data.length % limit))
       return {
         ...el,
         nodeRef: createRef<HTMLDivElement>(),
@@ -17,7 +15,7 @@ export const postDataEdit = <T>(data: Array<T>, limit: number) => {
         pageValue: nextPage,
       };
 
-    if (!(i % 5) && i < data.length - endIndex)
+    if (!(i % 5) && i < data.length - 1)
       return {
         ...el,
         nodeRef: createRef<HTMLDivElement>(),
