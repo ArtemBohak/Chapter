6c4a2d7320a57a21474449a9bb6f6d614a01a51b
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-03-17 22:52:34 +0200

    refactor: pagination logic

--- src/utils/callBacks/helpers/helpers.ts
+++ src/utils/callBacks/helpers/helpers.ts
@@ -1,10 +1,12 @@
 import { createRef } from "react";
+import { pageLimit } from "../..";
 
 export const dataFindIndex = <T>(array: Array<T>, obj: T, key: keyof T) =>
   array.findIndex((el) => el[key] === obj[key]);
 
 export const postDataEdit = <T>(data: Array<T>, limit: number) => {
   return data.map((el, i) => {
+    const endIndex = limit === pageLimit ? 1 : 5;
     const nextPage = Math.round(data.length / limit) + 1;
     const currentPage = Math.floor(i / limit) + 1;
     if (i === data.length - 1 && !(data.length % limit))
@@ -15,7 +17,7 @@ export const postDataEdit = <T>(data: Array<T>, limit: number) => {
         pageValue: nextPage,
       };
 
-    if (!(i % 5) && i < data.length - 1)
+    if (!(i % 5) && i < data.length - endIndex)
       return {
         ...el,
         nodeRef: createRef<HTMLDivElement>(),
