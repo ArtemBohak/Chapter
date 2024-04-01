88d09909f5cb57ffa9d710f048548aad216592fe
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-04-01 11:07:54 +0300

    feat: this add delete cloudinary book image functional

--- src/components/BookShelf/EditBookModal/EditBookForm/EditBookForm.type.ts
+++ src/components/BookShelf/EditBookModal/EditBookForm/EditBookForm.type.ts
@@ -2,17 +2,18 @@ import { Dispatch } from "react";
 import { IBook } from "../../Book/BookProps.type";
 
 export type editBookProps = {
-    bookId?: number,
-    book_statusId: number,
-    nameOfBook: string,
-    author: string,
-    annotation: string,
-    imagePath: string,
-    favorite_book_status: boolean;
-}
+  bookId?: number;
+  book_statusId: number;
+  nameOfBook: string;
+  author: string;
+  annotation: string;
+  imagePath: string;
+  favorite_book_status: boolean;
+};
 
 export type EditBookFormProps = {
-    currentBook: IBook | undefined;
-    bookId: number,
-    setIsOpen: Dispatch<React.SetStateAction<boolean>>;
-}
+  currentBook: IBook | undefined;
+  bookId: number;
+  prevBookImagePath?: string;
+  setIsOpen: Dispatch<React.SetStateAction<boolean>>;
+};
