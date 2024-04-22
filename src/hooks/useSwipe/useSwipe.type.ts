88a30114f1768656ab6f5d9e4250665de95632d7
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-04-22 14:23:55 +0300

    revert changes of useSwipe hook

--- src/hooks/useSwipe/useSwipe.type.ts
+++ src/hooks/useSwipe/useSwipe.type.ts
@@ -2,7 +2,7 @@ import { ModalProps } from "@/src/components/Modal/Modal.type";
 import { RefType } from "@/src/types";
 
 export type UseSwipeProps = {
-  nodeRef: RefType;
+  nodeRef?: RefType;
   leftSwipeCB?: () => void;
   rightSwipeCB?: () => void;
 } & Pick<
