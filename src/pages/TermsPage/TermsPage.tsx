3d27ef6890c5e643d383bd2c748c6138b99dfb99
Andrii Valenko <andreyvalenko@gmail.com>
2024-05-02 13:46:19 +0300

    feat: this add terms page

--- src/pages/TermsPage/TermsPage.tsx
+++ src/pages/TermsPage/TermsPage.tsx
@@ -2,20 +2,23 @@ import { FC } from "react";
 import { Outlet } from "react-router-dom";
 import { NavigationTogglerProvider } from "@/src/context";
 import styles from "./TermsPage.module.scss";
-import { SidebarNavigation } from "./components";
+import { Header, SidebarNavigation } from "./components";
 
 const TermsPage: FC = () => {
   return (
     <>
       <NavigationTogglerProvider>
         <SidebarNavigation />
+        <Header />
       </NavigationTogglerProvider>
       <main>
         <section className={styles["terms"]}>
-          <h1 className={styles["title"]}>
-            Terms & Conditions and Privacy Policy
-          </h1>
-          <Outlet />
+          <div className={styles["terms__container"]}>
+            <h1 className={styles["title"]}>
+              Terms & Conditions and Privacy Policy
+            </h1>
+            <Outlet />
+          </div>
         </section>
       </main>
     </>
--- src/pages/TermsPage/components/NavigationList/NavigationList.tsx
+++ src/pages/TermsPage/components/NavigationList/NavigationList.tsx
@@ -3,18 +3,19 @@ import { NavLink, useLocation, useNavigate } from "react-router-dom";
 import cn from "classnames";
 
 import { useNavigationToggler } from "@/src/context";
-
-import { NavigationListProps } from "./NavigationList.type";
-import "./NavigationList.scss";
-
-import { Icon, IconEnum } from "../../../../components/Icon";
 import { links } from "@/src/types";
+import { useModal } from "@/src/hooks";
+import { NavigationListProps } from "./NavigationList.type";
+
+import "./NavigationList.scss";
+import { ContactUs, Icon, IconEnum } from "@/src/components";
 
 const NavigationList: FC<NavigationListProps> = ({
   items,
   className,
   isBottom = false,
 }) => {
+  const modalProps = useModal();
   const location = useLocation();
   const navigate = useNavigate();
   const { setIsActiveMenu } = useNavigationToggler();
@@ -24,58 +25,62 @@ const NavigationList: FC<NavigationListProps> = ({
   }
 
   return (
-    <ul className={cn("navigation-list", className)}>
-      {isBottom && (
-        <li className="navigation-list__item contact-us">
-          <button
-            className="navigation-list__link navigation-list__button"
-            aria-label="Open modal button"
-          >
-            <Icon
-              icon={IconEnum.EditBook}
-              className="navigation-list__link-icon"
-            />
-            Contact us
-          </button>
-        </li>
-      )}
-      {items.map((navItem) => (
-        <li key={navItem.id} className="navigation-list__item">
-          <NavLink
-            to={navItem.path}
-            aria-label="Menu nav link"
-            className={cn("navigation-list__link", {
-              "current-page": navItem.path === location.pathname,
-            })}
-            onClick={handleClickNavLink}
-          >
-            {navItem.icon ? (
+    <>
+      <ul className={cn("navigation-list", className)}>
+        {isBottom && (
+          <li className="navigation-list__item contact-us">
+            <button
+              className="navigation-list__link navigation-list__button"
+              aria-label="Open modal button"
+              onClick={() => modalProps.setActive(true)}
+            >
               <Icon
-                icon={navItem.icon}
+                icon={IconEnum.EditBook}
                 className="navigation-list__link-icon"
               />
-            ) : null}
-            {navItem.name}
-          </NavLink>
-        </li>
-      ))}
-      {isBottom && (
-        <li className="navigation-list__item">
-          <button
-            onClick={() => navigate(links.HOME)}
-            className="navigation-list__link navigation-list__button"
-            aria-label="nav button"
-          >
-            <Icon
-              icon={IconEnum.ArrowBack}
-              className="navigation-list__link-icon navigation-list__custom-icon"
-              removeInlineStyle
-            />
-            Back
-          </button>
-        </li>
-      )}
-    </ul>
+              Contact us
+            </button>
+          </li>
+        )}
+        {items.map((navItem) => (
+          <li key={navItem.id} className="navigation-list__item">
+            <NavLink
+              to={navItem.path}
+              aria-label="Menu nav link"
+              className={cn("navigation-list__link", {
+                "current-page": navItem.path === location.pathname,
+              })}
+              onClick={handleClickNavLink}
+            >
+              {navItem.icon ? (
+                <Icon
+                  icon={navItem.icon}
+                  className="navigation-list__link-icon"
+                />
+              ) : null}
+              {navItem.name}
+            </NavLink>
+          </li>
+        ))}
+        {isBottom && (
+          <li className="navigation-list__item">
+            <button
+              onClick={() => navigate(links.HOME)}
+              className="navigation-list__link navigation-list__button"
+              aria-label="nav button"
+            >
+              <Icon
+                icon={IconEnum.ArrowBack}
+                className="navigation-list__link-icon navigation-list__custom-icon"
+                removeInlineStyle
+              />
+              Back
+            </button>
+          </li>
+        )}
+      </ul>
+      <ContactUs {...modalProps} />
+    </>
   );
 };
 
